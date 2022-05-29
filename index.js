94890cafb4c10882cd7fa119e87229a8c68d248b
hanungpuji <hanung@ilcs.co.id>
2022-05-29 19:15:26 +0700

    challenge 02

--- index.js
+++ index.js
@@ -17,25 +17,42 @@ function lakukanLooping(arrPegawai) {
   // ! JANGAN DIMODIFIKASI
   let dataYangAkanDilooping = arrPegawai;
 
+  let jmlData = dataYangAkanDilooping.length;
+
   /*
     TODO 1: Buatlah sebuah variabel bernama "hasilLooping" 
       yang berisi gabungan nama depan dan belakang dari masing masing pegawai
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
+
+  let hasilLooping = [];
+  let dataPria = [];
+  let dataWanita = [];
+  for (let i = 0; i < jmlData ; i++) {
+    hasilLooping.push(dataYangAkanDilooping[i]["namaDepan"]+" "+dataYangAkanDilooping[i]["namaBelakang"]);
+
+    switch (dataYangAkanDilooping[i]["jenisKelamin"]){
+      case 'M':
+        dataPria.push(dataYangAkanDilooping[i]["namaDepan"]+" "+dataYangAkanDilooping[i]["namaBelakang"]);
+        break;
+      case 'F':
+        dataWanita.push(dataYangAkanDilooping[i]["namaDepan"]+" "+dataYangAkanDilooping[i]["namaBelakang"]);
+        break;
+    }
+  }
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
-  let jumlahPria = null;
+  let jumlahPria = dataPria.length;
 
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
-  let jumlahWanita = null;
+  let jumlahWanita = dataWanita.length;
 
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
@@ -48,8 +65,9 @@ function lakukanLooping(arrPegawai) {
       Apabila imbang, maka komentar akan berisi:
         "Jumlah Pria dan Wanita berimbang"
   */
-  let komentar = null;
+  let komentar = jumlahPria > jumlahWanita ? "Jumlah Pria lebih banyak dari Wanita" : jumlahPria < jumlahWanita ? "Jumlah Wanita lebih banyak dari Pria" : "Jumlah Pria dan Wanita berimbang";
 
+  console.log(komentar);
   // ! JANGAN DIMODIFIKASI
   return {
     hasilLooping,
